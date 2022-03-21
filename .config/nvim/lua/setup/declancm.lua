703379303f8fac39da3db8d993f3d4d640639ac6
declancm <declanmullen3.14@gmail.com>
2022-03-21 20:04:25 +1100

    Auto Commit: Mar 21 09:04:25 2022 UTC

--- .config/nvim/lua/setup/declancm.lua
+++ .config/nvim/lua/setup/declancm.lua
@@ -7,18 +7,19 @@ local keymap = vim.keymap.set
 
 -- require('git-scripts').setup {
 --   default_keymaps = false,
---   commit_on_save = true,
---   warnings = false,
+--   -- commit_on_save = true,
+--   -- warnings = false,
 -- }
 
 -- CINNAMON-SCROLL:
 
--- require('cinnamon').setup {
---   default_keymaps = false,
---   extra_keymaps = true,
---   centered = false,
---   scroll_limit = 50,
--- }
+require('cinnamon').setup {
+  -- default_keymaps = false,
+  extra_keymaps = true,
+  -- centered = false,
+  -- disable = true,
+  -- scroll_limit = 50,
+}
 
 -- keymap({ 'n', 'x' }, 'gg', "<Cmd>lua Cinnamon.Scroll('gg', 0, 0, 3)<CR>", opts)
 -- keymap({ 'n', 'x' }, 'G', "<Cmd>lua Cinnamon.Scroll('G', 0, 1, 3)<CR>", opts)
