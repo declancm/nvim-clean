468742c8d624975008aecaacadb6ee3455352767
Declan Mullen <declanmullen3.14@gmail.com>
2024-03-20 15:52:00 -0700

    Removed config breaking telescope fzf

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -55,10 +55,7 @@ require("lazy").setup({
     dependencies = {
       {
         'nvim-telescope/telescope-fzf-native.nvim',
-        build = 'make',
-        config = function()
-          require("telescope").load_extension("fzf")
-        end,
+        build = 'make'
       },
     },
   },
