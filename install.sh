f4489a671ccb978f12e2e2a7a60aad6071461261
Declan Mullen <declanmullen3.14@gmail.com>
2024-03-19 22:22:36 -0700

    Use the '-y' argument for all apt install commands

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update
 sudo apt update && sudo apt upgrade -y
-sudo apt install cargo python3-pip
+sudo apt install cargo python3-pip -y
 
 # install nodejs
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
@@ -12,10 +12,10 @@ nvm install 21.7.1
 npm install -g node-gyp
 
 # clipboard
-sudo apt install xsel
+sudo apt install xsel -y
 
 # lsp servers
-sudo apt install clangd-12
+sudo apt install clangd-12 -y
 npm install bash-language-server
 npm install vscode-langservers-extracted
 npm install typescript typescript-language-server
@@ -29,10 +29,10 @@ npm install --save-dev --save-exact prettier
 cargo install stylua
 
 # telescope
-sudo apt install ripgrep
-sudo apt install fd-find
-sudo apt install fzf
-sudo apt install zoxide
+sudo apt install ripgrep -y
+sudo apt install fd-find -y
+sudo apt install fzf -y
+sudo apt install zoxide -y
 cargo install zoxide --locked
 
 # treesitter
