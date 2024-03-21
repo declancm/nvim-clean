a7bfb3901324599ae9e51ed0aa1ce0da67cbe8cb
Declan Mullen <declanmullen3.14@gmail.com>
2024-03-21 12:24:40 -0700

    Simplify lualine config

--- .config/nvim/lua/config/visuals.lua
+++ .config/nvim/lua/config/visuals.lua
@@ -66,15 +66,12 @@ end
 
 lualine.setup {
   options = {
-    theme = theme,
-    component_separators = { left = '', right = '' },
-    section_separators = { left = '', right = '' },
-    globalstatus = true,
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
   },
   sections = {
-    lualine_a = { 'mode' },
-    lualine_b = { 'branch' },
-    lualine_c = {
+    lualine_b = {
+      'branch',
       'diff',
       {
         'diagnostics',
@@ -87,21 +84,8 @@ lualine.setup {
         },
       },
     },
-    lualine_x = {
-      { navic.get_location, cond = navic.is_available },
-    },
-    lualine_y = { 'progress' },
-    lualine_z = { 'location' },
   },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = { 'filename' },
-    lualine_x = { 'location' },
-    lualine_y = {},
-    lualine_z = {},
-  },
-  tabline = {},
+
   extensions = { 'fugitive' },
 }
 
@@ -208,10 +192,8 @@ incline.setup {
       end
 
       return {
-        { '', guibg = 'none', guifg = color },
-        { ' ' .. head, guibg = color, guifg = color3 },
-        { tail .. ' ', guibg = color },
-        { '', guibg = 'none', guifg = color },
+        { head, guibg = color, guifg = color3 },
+        { tail, guibg = color },
       }
     end
 
