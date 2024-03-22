659f735b12795d8556bd5ba2c5a3fc0832e543ef
Declan Mullen <declanmullen3.14@gmail.com>
2024-03-21 17:45:07 -0700

    Specify lazygit in the dependency installer

--- install-deps.sh
+++ install-deps.sh
@@ -41,6 +41,6 @@ cargo install zoxide --locked
 # treesitter
 npm install tree-sitter
 
-# git
+# lazygit
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lo ~/lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz" && tar xf lazygit.tar.gz -C ~/ && sudo install ~/lazygit /usr/local/bin
