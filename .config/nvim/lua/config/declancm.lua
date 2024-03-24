8988da2ee6722e3111f5f5906a33ccccd4b063e7
Declan Mullen <declanmullen3.14@gmail.com>
2024-03-24 11:58:31 -0700

    Only use cinnamon default keymaps

--- .config/nvim/lua/config/declancm.lua
+++ .config/nvim/lua/config/declancm.lua
@@ -7,17 +7,11 @@ local keymap = vim.keymap.set
 -- CINNAMON-SCROLL:
 
 require('cinnamon').setup {
-  extra_keymaps = true,
-  extended_keymaps = true,
-  override_keymaps = true,
-  -- hide_cursor = true,
-  max_length = 500,
-  scroll_limit = 500,
+  default_delay = 5,
 }
 
-keymap({ 'n', 'x' }, 'gg', "<Cmd>lua Scroll('gg', 0, 0, 3)<CR>")
-keymap({ 'n', 'x' }, 'G', "<Cmd>lua Scroll('G', 0, 1, 3)<CR>")
--- keymap('n', 'gd', "<Cmd>lua Cinnamon.Scroll('definition')<CR>")
+-- keymap({ 'n', 'x' }, 'gg', "<Cmd>lua Scroll('gg', 0, 0, 3)<CR>")
+-- keymap({ 'n', 'x' }, 'G', "<Cmd>lua Scroll('G', 0, 1, 3)<CR>")
 
 -- WINDEX:
 
@@ -28,4 +22,3 @@ require('windex').setup {
 
 -- Open lazygit:
 keymap('n', '<C-g>', "<Cmd>lua require('windex').toggle_terminal('all', 'lazygit')<CR>")
-
